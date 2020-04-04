6dd0b04294baa05bac0ad387050149d1a729ea2f
Yadu Nand B <yadudoc1729@gmail.com>
2020-04-04 17:50:17 -0500

    Adding frontera configs

---
+++ scripts/frontera_small.py
@@ -0,0 +1,38 @@
+from parsl.config import Config
+from parsl.channels import LocalChannel
+from parsl.providers import SlurmProvider
+from parsl.executors import HighThroughputExecutor
+from parsl.launchers import SrunLauncher
+from parsl.addresses import address_by_hostname
+
+
+""" This config assumes that it is used to launch parsl tasks from the login nodes
+of Frontera at TACC. Each job submitted to the scheduler will request 2 nodes for 10 minutes.
+"""
+config = Config(
+    executors=[
+        HighThroughputExecutor(
+            label="frontera_htex",
+            address=address_by_hostname(),
+            max_workers=30,          # Set number of workers per node
+            provider=SlurmProvider(
+                cmd_timeout=120,     # Add extra time for slow scheduler responses
+                nodes_per_block=40,
+                walltime='02:00:00',
+                partition='development',                                 # Replace with partition name
+
+                init_blocks=3,
+                min_blocks=1,
+                max_blocks=3,
+
+                scheduler_options='''#SBATCH -A FTA-Jha''',   # Enter scheduler_options if needed
+
+                # Command to be run before starting a worker, such as:
+                # 'module load Anaconda; source activate parsl_env'.
+                worker_init='source ~/anaconda3/bin/activate; conda activate candle_py3.7',
+                launcher=SrunLauncher(),
+            ),
+        )
+    ],
+    strategy=None,
+)
